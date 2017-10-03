4938ac4ebdfcc7f7c293fbcad5954a2d028cff35
serqbond <serqbond@gmail.com>
2017-10-03 13:18:33 +0300

    Add test play with DateTime

--- src/test/java/serhii/test/AllStatesTest.java
+++ src/test/java/serhii/test/AllStatesTest.java
@@ -7,6 +7,11 @@ import com.google.gson.*;
 import io.restassured.RestAssured;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import static io.restassured.RestAssured.get;
 import static org.hamcrest.CoreMatchers.is;
@@ -55,5 +60,20 @@ public class AllStatesTest extends FunctionalTest{
         RestAssured rest =  new RestAssured();
         rest.baseURI = "http://api.openweathermap.org";
         rest.get("/data/2.5/weather?lat=35&lon=139").then().statusCode(401);
+    }
+
+    @Test
+    public void playDateTime(){
+        System.out.println("playDateTime " + Thread.currentThread().getName());
+
+        LocalDateTime time = LocalDateTime.now();
+
+        Assert.assertTrue("withSecond doesn't work. Expectd 5 , but was " + time.withSecond(5).getSecond(), time.withSecond(5).getSecond() == 5);
+      
+        String timeString = "2019-10-03T00:00";
+        LocalDateTime fromStr = LocalDateTime.parse(timeString);
+        LocalDateTime newFromStr = fromStr.plusHours(13).plusMinutes(86).plusSeconds(96).minusYears(2);
+
+        Assert.assertFalse("Current year - "  + newFromStr.getYear() + " is not a leap year!" , newFromStr.toLocalDate().isLeapYear());
     }
 }
