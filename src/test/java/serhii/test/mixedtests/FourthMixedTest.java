baae561811d3fcee03671a3805b4628b9811088d
serqbond <serqbond@gmail.com>
2017-10-25 23:32:19 +0300

    run in suits

---
+++ src/test/java/serhii/test/mixedtests/FourthMixedTest.java
@@ -0,0 +1,41 @@
+package serhii.test.mixedtests;
+
+import businessentities.serhii.be.allcountries.AllCountries;
+import com.google.gson.Gson;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import serhii.test.BaseTest;
+import uicontext.googlesearchcontext.GoogleSearchContext;
+
+import java.util.Arrays;
+
+import static io.restassured.RestAssured.given;
+
+@Test(suiteName = "Fourth Mixed Test")
+public class FourthMixedTest extends BaseTest{
+    private final String basePath = "/country";
+
+    @Test(description = "Search For Piano Test")
+    public void searchForPianoTest() {
+
+        System.out.println("searchForPianoTest with REST " + Thread.currentThread().getName());
+
+        GoogleSearchContext context = new GoogleSearchContext(driver);
+
+        context
+                .opensTheSearchApp(baseUrl)
+                .searchesFor("Piano")
+                .shouldSeeTitle("Piano - Поиск в Google");
+
+        String response = given(requestSpecification).get(basePath + "/get/all").body().asString();
+        Gson gson = new Gson();
+        AllCountries stateListResponse = gson.fromJson(response, AllCountries.class);
+        Assert.assertEquals(Arrays.stream(stateListResponse.getRestResponse().getResult())
+                        .filter(country -> country.getName().contains("Ukraine"))
+                        .findFirst()
+                        .get()
+                        .getAlpha2_code(),
+                "UA"
+        );
+    }
+}
---
+++ src/test/java/serhii/test/mixedtests/ThirdMixedTest.java
@@ -0,0 +1,41 @@
+package serhii.test.mixedtests;
+
+import businessentities.serhii.be.allcountries.AllCountries;
+import com.google.gson.Gson;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import serhii.test.BaseTest;
+import uicontext.googlesearchcontext.GoogleSearchContext;
+
+import java.util.Arrays;
+
+import static io.restassured.RestAssured.given;
+
+@Test(suiteName = "Third Mixed Test")
+public class ThirdMixedTest extends BaseTest{
+    private final String basePath = "/country";
+
+    @Test(description = "Search For Banana Test 2")
+    public void searchForBananaTest2() {
+
+        System.out.println("searchForBananaTest2 with REST " + Thread.currentThread().getName());
+
+        GoogleSearchContext context = new GoogleSearchContext(driver);
+
+        context
+                .opensTheSearchApp(baseUrl)
+                .searchesFor("banana")
+                .shouldSeeTitle("banana - Поиск в Google");
+
+        String response = given(requestSpecification).get(basePath + "/get/all").body().asString();
+        Gson gson = new Gson();
+        AllCountries stateListResponse = gson.fromJson(response, AllCountries.class);
+        Assert.assertEquals(Arrays.stream(stateListResponse.getRestResponse().getResult())
+                        .filter(country -> country.getName().contains("Ukraine"))
+                        .findFirst()
+                        .get()
+                        .getAlpha2_code(),
+                "UA"
+        );
+    }
+}
