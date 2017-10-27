e83116a92749e1855f4a37424f665e3a934d75f7
serqbond <serqbond@gmail.com>
2017-10-27 13:22:40 +0300

    change color to blue

--- src/main/java/utilities/Utilities.java
+++ src/main/java/utilities/Utilities.java
@@ -8,7 +8,7 @@ public class Utilities {
 
     public static void Highlight(WebDriver driver, WebElement element){
         if (driver instanceof JavascriptExecutor) {
-            ((JavascriptExecutor)driver).executeScript("arguments[0].style.border='3px solid red'", element);
+            ((JavascriptExecutor)driver).executeScript("arguments[0].style.border='3px solid blue'", element);
         }
     }
 
